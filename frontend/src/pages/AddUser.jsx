194553991170812a7595ce4cf975bba34391bafd
erik1498 <djefrisonferdinan@gmail.com>
2023-08-09 00:23:18 +0700

    Adding Components And Features

---
+++ frontend/src/pages/AddUser.jsx
@@ -0,0 +1,11 @@
+import React     from "react";
+import FormAddUser from "../components/FormAddUser";
+import Layout from "./Layout";
+
+export default function AddUser() {
+    return (
+        <Layout>
+            <FormAddUser />
+        </Layout>
+    )
+}
